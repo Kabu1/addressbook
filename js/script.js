4e9c1862bc07e590c4286d617338bd66607bbf08
k <kkabubenson@gmail.com>
2020-06-24 12:18:47 -0400

    add styling effects to the page

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ function Contact(first, last, address, city){
     this.address = address;
     this.city = city;
 }
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
 
 
 //user interface logic
@@ -15,11 +18,18 @@ $(document).ready(function(){
 
         var newContact = new Contact(inputFirstName, inputLastName);
 
-        $("ul#contacts").append("<li><span class ='contact'>" + newContact.firstName + "</span></li>")
+        $("ul#contacts").append("<li><span class ='contact'>" + newContact.fullName() + "</span></li>")
 
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
 
+        $(".contact").last().click(function() {
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName, newContact.lastName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+          }); 
+    
         event.preventDefault();
     });
 });
