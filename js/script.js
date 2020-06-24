cd17645a84a8b649100434199579922eb7986596
k <kkabubenson@gmail.com>
2020-06-24 16:59:35 -0400

    final commit

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ function Contact(first, last, address, city){
     this.address = address;
     this.city = city;
 }
-Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName;
+Contact.prototype.fullDetails = function() {
+    return this.firstName + " " + this.lastName+ " " + this.address + " "+ this.city;
   }
 
 
@@ -15,19 +15,27 @@ $(document).ready(function(){
     $("#new-contact").submit(function(event){
         var inputFirstName = $("input#new-first-name").val();
         var inputLastName = $("input#new-last-name").val();
+        var inputAddress= $("input#address").val();
+        var inputCity = $("input#city").val();
 
-        var newContact = new Contact(inputFirstName, inputLastName);
 
-        $("ul#contacts").append("<li><span class ='contact'>" + newContact.fullName() + "</span></li>")
+        var newContact = new Contact(inputFirstName, inputLastName,inputAddress, inputCity);
+
+        $("ul#contacts").append("<li><span class ='contact'>" + newContact.firstName + " " +newContact.lastName + "</span> </li>")
 
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
+        $("input#address").val("");
+        $("input#city").val("");
 
         $(".contact").last().click(function() {
             $("#show-contact").show();
-            $("#show-contact h2").text(newContact.firstName, newContact.lastName);
+            $("#show-contact h2").text(newContact.firstName);
             $(".first-name").text(newContact.firstName);
             $(".last-name").text(newContact.lastName);
+            $(".address").text(newContact.address);
+            $(".city").text(newContact.city);
+
           }); 
     
         event.preventDefault();
